051819fd930c150bdf2e13e0f59eaf2dfb9ddb75
Gabriel <ahrensgabriel09@gmail.com>
2022-08-20 17:55:53 -0300

    Add new action (getTenRegisteredOperatons) and its respective actionType

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { CURRENT_BALANCE } from './actions';
+import { CURRENT_BALANCE, GET_TEN_REGISTERED_OPERATIONS } from './actions';
 
 function createOperation(data) {
     return async (dispatch) => {
@@ -11,6 +11,39 @@ function createOperation(data) {
         }
     }
 }
+
+function getCurrentBalance() {
+    return async (dispatch) => {
+        try {
+            const balance = await axios.get('http://localhost:3001/currentBalance')
+            return dispatch({
+                type: CURRENT_BALANCE,
+                payload: balance.data
+            })
+        } catch (e) {
+            alert(e.response.data)
+            
+        }
+    }
+}
+
+function getTenRegisteredOperatons() {
+    return async (dispatch) => {
+        try {
+            const operations = await axios.get('http://localhost:3001/tenRegisteredOperations')
+            return dispatch({
+                type: GET_TEN_REGISTERED_OPERATIONS,
+                payload: operations.data
+            })
+        }
+        catch (e) {
+            
+        }
+    } 
+}
+
 export {
     createOperation,
+    getCurrentBalance,
+    getTenRegisteredOperatons
 }
