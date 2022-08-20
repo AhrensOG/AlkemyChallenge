80cf630c60aba1d4dc19d46885980d244df61139
Gabriel <ahrensgabriel09@gmail.com>
2022-08-20 18:32:41 -0300

    Add new Route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import CreateOperation from './components/CreateOperation'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <div className="App">
           <Routes>
             <Route path="/home" element={<Home/>}/>
+            <Route path='/createOperation' element={<CreateOperation/>}></Route>
           </Routes>
         </div>
     </BrowserRouter>
