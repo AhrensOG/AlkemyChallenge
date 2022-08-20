33f7e8e7f946462e921268d7109ce3ff37ef4367
Gabriel <ahrensgabriel09@gmail.com>
2022-08-20 17:58:35 -0300

    Add new state(operations) for list operations

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,6 +1,8 @@
-import { CREATE_OPERATION } from "../actions/actions";
+import { CREATE_OPERATION, CURRENT_BALANCE, GET_TEN_REGISTERED_OPERATIONS } from "../actions/actions";
 
 const initialState = {
+  currentBalance: 0, 
+  operations: [],
 };
 
 function rootReducer(state = initialState, action) {
@@ -8,6 +10,16 @@ function rootReducer(state = initialState, action) {
     case CREATE_OPERATION:
       return {
         ...state,
+      }
+    case CURRENT_BALANCE:
+      return {
+        ...state,
+        currentBalance: action.payload
+      }
+    case GET_TEN_REGISTERED_OPERATIONS:
+      return {
+        ...state,
+        operations: action.payload
       }
     default:
       return state;
